df831f1aac1b41ff5757eb165952b415f8ebfc21
Owen Lynch <root@owenlynch.org>
2022-11-15 10:31:07 -0800

    massive simplification

--- flake.nix
+++ flake.nix
@@ -1,15 +1,6 @@
 {
-  inputs = {
-    nixpkgs = "nixpkgs";
-  };
-
   outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
+    {
       nixosModules.default = import ./module.nix;
     };
 }
