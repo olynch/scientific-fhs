53f7dba0e0ea683d1b6f0a24a479e15ce05c59a5
Owen Lynch <root@owenlynch.org>
2023-04-27 11:30:59 -0700

    fixed quarto hash

--- quarto.nix
+++ quarto.nix
@@ -68,5 +68,5 @@ in {
   rstudio = makeRstudio "2022.02.2-485"
     "995a85a058cbb0f59675399ed30a9f39f62167d5558ac6c0fd4459111b09d7d5";
   quarto = makeQuarto "1.3.340"
-    "sha256-liZc7Ewo7HaIXdcXp3232SW5JlOmZiZDawusjgJt8pE=";
+    "sha256-zGzn0nXTBghgVPP2zX7hN0vteTGjIj6JziRODOd4N/U=";
 }
