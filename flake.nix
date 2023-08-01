464e6e93eda77a93612b182c1066596da316d9e3
Owen Lynch <root@owenlynch.org>
2023-08-01 14:59:31 -0400

    added package

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
 {
   outputs = { self, nixpkgs, ... }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    fhsCommand = pkgs.callPackage ./fhs.nix {
+      enableNVIDIA = false;
+      juliaVersion = "1.9.2";
+    };
+  in
     {
       nixosModules.default = import ./module.nix;
+      packages.x86_64-linux.julia = fhsCommand "julia" "julia";
     };
 }
