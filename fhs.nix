f887c5049f8193586340e1c51e33466299d0e173
Matt Camp <matt@aicampground.com>
2024-02-08 15:48:47 -0600

    wip

--- fhs.nix
+++ fhs.nix
@@ -173,6 +173,20 @@ let
     export CUDA_PATH=${pkgs.cudatoolkit_11}
     export LD_LIBRARY_PATH=${pkgs.cudatoolkit_11}/lib:${pkgs.cudnn_cudatoolkit_11}/lib:${pkgs.cudatoolkit_11.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH
     export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+                    pkgs.glib
+                    pkgs.linuxPackages.nvidia_x11
+                    pkgs.libz
+                    pkgs.libGL
+                    pkgs.stdenv.cc.cc
+                  ]}
+    #export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudatoolkit.lib}/lib:${pkgs.cudatoolkit}/lib
+    #export RUNPATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudatoolkit}/lib/stubs:${pkgs.cudatoolkit_11}/lib:${pkgs.cudatoolkit_11.lib}/lib:$LD_LIBRARY_PATH
+    #export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib:${pkgs.stdenv.cc.cc.lib}/lib
+    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
   '';
 
   envvars = std_envvars + optionalString enableGraphical graphical_envvars
